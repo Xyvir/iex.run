48210661613b723b634d73d260b40044f5a58f8d
Xyvir <tymills620@gmail.com>
2022-04-18 15:01:55 -0500

    Add files via upload

---
+++ alphabet.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+SETLOCAL EnableExtensions  EnableDelayedExpansion
+REM accepts 1 parameter, Type ascii numerical ranges with dash separated by spaces double-quoted.
+REM All characters are between 33-126, numbers 49-57, lower 97-122, upper 65-90
+set ipt=%1
+if not defined ipt set ipt="97-122 65-90"
+set ipt=%ipt:~1,-1%
+FOR %%g in (%ipt%) DO (
+set var=%%g & set var=!var:-= !
+set rng=!rng!ECHO !var! ^^^^^^^^^^^^^^^&)
+set rng=%rng:~0,-5%
+FOR /F "tokens=1,2" %%a  IN ('%rng%') DO (
+set up=1 & if %%a GTR %%b set up=-1
+FOR /L %%i IN (%%a,!up!,%%b) DO (
+cmd /c exit %%i & set alp=!alp!!=exitcodeAscii! ))
+echo !alp!
