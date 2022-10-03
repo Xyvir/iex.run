6bc6bc847e682b8a2b839ae36ffc3f2abd6809b2
Xyvir <tymills620@gmail.com>
2022-10-03 14:41:01 -0500

    Update alphabet.cmd

--- alphabet.cmd
+++ alphabet.cmd
@@ -2,9 +2,10 @@
 SETLOCAL EnableExtensions  EnableDelayedExpansion
 REM accepts 1 parameter, Type ascii numerical ranges with dash separated by spaces double-quoted.
 REM All characters are between 33-126, numbers 49-57, lower 97-122, upper 65-90
-set "ipt=%*"
-if not defined ipt set "ipt=97-122 65-90"
-FOR %%g in ("%ipt%") DO (
+set "ipt=%* "
+set ipt=%ipt:~1,-1%
+if not defined ipt set ipt="97-122 65-90"
+FOR %%g in (%ipt%) DO (
 set var=%%g & set var=!var:-= !
 set rng=!rng!ECHO !var! ^^^^^^^^^^^^^^^&)
 set rng=%rng:~0,-5%
