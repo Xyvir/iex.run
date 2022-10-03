11694c562d96de1ad0748cf8178df10771d1f06c
Xyvir <tymills620@gmail.com>
2022-10-03 13:28:26 -0500

    Rename officerepair.bat to officerepair.cmd

--- officerepair.bat
+++
@@ -1,5 +0,0 @@
-REM Get Office Bitness
-FOR /f "TOKENS=3" %%A in ('REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\ClickToRun\Configuration /v Platform') do SET "BITNESS=%%A"
-
-REM Run repair
-echo Start-Process -FilePath "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeClickToRun.exe" -wait -verb RunAs -ArgumentList "scenario=Repair platform=%BITNESS% culture=en-us forceappshutdown=True RepairType=FullRepair DisplayLevel=True" | powershell -c "-"
