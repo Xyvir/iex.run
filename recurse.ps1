62a6c5c37c71d9f39409879cfbcaa830840303ed
Xyvir <tymills620@gmail.com>
2022-10-03 21:09:24 -0500

    Update recurse.ps1

--- recurse.ps1
+++ recurse.ps1
@@ -1,4 +1,6 @@
-# in scripting engine call 'curl.exe iex.run/recurse | iex' to run the following:
-# Use out-null to wait between steps or w/o to run asynchronously 
+# recurse.ps1; example of how you can call iex.run-hosted scripts from other scripts.
+# in your scripting engine call 'curl.exe iex.run/recurse | iex' to also run alphabet.cmd and paramtest.cmd in order.
+$testvar = "Hello World"
 iex.run alphabet 
 iex.run paramtest?1
+iex.run passvar
