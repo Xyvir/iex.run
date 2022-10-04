a1cf81a5104f6c2c2647d0a8b3787d4f9a4969d2
Xyvir <tymills620@gmail.com>
2022-10-03 20:06:33 -0500

    Update recurse.ps1

--- recurse.ps1
+++ recurse.ps1
@@ -1,4 +1,3 @@
 # in scripting engine call 'curl.exe iex.run/recurse | iex' to run the following:
 # Use out-null to wait between steps or w/o to run asynchronously 
-iex.run alphabet | out-null
-iex.run paramtest?1 | out-null
+iex.run alphabet | out-null; iex.run paramtest?1 | out-null
