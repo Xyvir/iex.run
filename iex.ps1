9a1ea09cfb7560bb6e98660fed9266ad45b67a95
Xyvir <tymills620@gmail.com>
2022-10-14 08:47:49 -0500

    Update iex.ps1

--- iex.ps1
+++ iex.ps1
@@ -138,7 +138,7 @@ If (!($DownloadUrl)) {
   Write-Host "No scripts matching '$command' found in $github, trying a built-in command`n" -ForegroundColor Yellow
   try {start-process -nonewwindow -wait powershell -ArgumentList "-command `"& $command $arguments`" "}
   catch { Write-Host "No built-in matches found, please double-check your spelling" -ForegroundColor Red }
-  finally {$internal = $true}
+  finally {$internal = $true; write-host ""}
   }
  if (!($internal)) { 
    Write-Host "Available Files and Status :" -ForegroundColor Yellow 
