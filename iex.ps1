e6e6b368f925e930bc296fd980270e0a0be7da25
Xyvir <tymills620@gmail.com>
2022-10-24 11:44:51 -0500

    Update iex.ps1

--- iex.ps1
+++ iex.ps1
@@ -211,7 +211,7 @@ if (!($error)) {Write-Host ("$exe $github Complete!").trim(" ") -ForegroundColor
 if (!($_NoClipboard))  {
 if ($exe -or $internal){
  Set-Clipboard ("https://" + $invoc)
- write-host "The following 'Magic URL' has been copied to your keyboard: https://$invoc" 
+ write-host "The following 'Magic URL' has been copied to your keyboard: https://$invoc `n" 
  }}
 
 if ($_DebugVars) {write-host ""; get-variable | where-object {(@("FormatEnumerationLimit", "MaximumAliasCount", "MaximumDriveCount", "MaximumErrorCount", "MaximumFunctionCount", "MaximumVariableCount", "PGHome", "PGSE", "PGUICulture", "PGVersionTable", "PROFILE", "PSSessionOption") -notcontains $_.name) -and (([psobject].Assembly.GetType('System.Management.Automation.SpecialVariables').GetFields('NonPublic,Static') | Where-Object FieldType -eq ([string]) | ForEach-Object GetValue $null)) -notcontains $_.name}}
