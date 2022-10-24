1a1c1b0915d82277dd645df2c1af3904029f18fc
Xyvir <tymills620@gmail.com>
2022-10-24 14:43:52 -0500

    Update recurse.ps1

--- scripts/recurse.ps1
+++ scripts/recurse.ps1
@@ -1,5 +1,5 @@
-# recurse.ps1; example of how you can call iex.run-hosted scripts from other scripts.
-# in your scripting engine call 'curl.exe iex.run/recurse | iex' to also run alphabet.cmd and paramtest.cmd in order.
+## recurse.ps1; example of how you can call iex.run-hosted scripts from other scripts.
+## in your scripting engine call 'curl.exe iex.run/recurse | iex' to also run alphabet.cmd and paramtest.cmd in order.
 $Env:testvar = "Hello World"
 iex.run alphabet
 iex.run paramtest?1
