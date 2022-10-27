0eb929d70bf13398bd40ecf0d4038719740f9f1e
Xyvir <tymills620@gmail.com>
2022-10-27 14:48:39 -0500

    Update palemoon.ps1

--- scripts/palemoon.ps1
+++ scripts/palemoon.ps1
@@ -3,5 +3,5 @@ $resp = Invoke-WebRequest $url -MaximumRedirection 0 -ErrorAction Ignore
 $Env:DLRemote = (($resp.RawContent -split  "//") | Select -last 1).split("`n") | select -first 1
 $Env:DLRemote = $Env:DLRemote.trim("`r")
 $Env:DLRemote = "https://$Env:DLRemote"
-iex.run "?-s"
+iex.run "?-y"
 Palemoon-Portable.exe
