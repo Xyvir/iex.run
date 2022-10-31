a731d3573ec9dd51182bca61593955ff155d01ec
Xyvir <tymills620@gmail.com>
2022-10-31 15:22:04 -0500

    Update opera.ps1

--- scripts/opera.ps1
+++ scripts/opera.ps1
@@ -1,1 +1,3 @@
-iex.run https%3A%2F%2Fninite.com%2Fopera%2Fninite.exe?@DLRemote
+try {iex.run https%3A%2F%2Fninite.com%2Fopera%2Fninite.exe?@DLRemote }
+catch ($Env:DLRemote = https://ninite.com/opera/ninite.exe; iex.run ?@DLRemote}
+finally {}
