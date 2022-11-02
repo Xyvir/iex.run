0dad01acc930a3e82af88ec943e35441f113b284
Xyvir <tymills620@gmail.com>
2022-11-02 12:48:01 -0500

    Update palemoon.ps1

--- scripts/palemoon.ps1
+++ scripts/palemoon.ps1
@@ -1,5 +1,5 @@
 $url = "https://www.palemoon.org/download.php?mirror=us&bits=64&type=portable"
-$resp = Invoke-WebRequest $url -MaximumRedirection 0 -ErrorAction Ignore
+$resp = Invoke-WebRequest -UseBasicParsing $url -MaximumRedirection 0 -ErrorAction Ignore
 $Env:DLRemote = "https://" + ((($resp.RawContent -split  "//") | Select -last 1).split("`n`r") | select -first 1)
 iex.run "?-y"
 Palemoon-Portable.exe
