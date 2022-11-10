50dce814f4b852c01a8f3d1dfa650bc08c45f99c
Xyvir <tymills620@gmail.com>
2022-11-10 10:02:48 -0600

    Update iex.ps1

--- iex.ps1
+++ iex.ps1
@@ -108,6 +108,8 @@ $env:Path += ";$_DownloadFolder;"
 if (!($_NoStub)) {
 $stub = @"
 @ECHO OFF
+ECHO 1 %CMDCMDLINE%
+ECHO 2 %CMDCMDLINE% ^| findstr /i %~n0
 FOR /F "USEBACKQ" %%A IN (``ECHO %CMDCMDLINE% ^| findstr /i %~n0``) do (set "pipe=1")
 if defined pipe set /p "p="
 if defined pipe call %p%
