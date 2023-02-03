d4cff89f22123ac3cc5db064455875c78cb4089d
Xyvir <tymills620@gmail.com>
2023-02-03 15:51:30 -0600

    Update iex.ps1

--- iex.ps1
+++ iex.ps1
@@ -216,10 +216,10 @@ if ($exe) {
     # If run as non-interactive system user, run as logged in user instead. Otherwise run in normal user context according to provided meta-parameneters
     if ( ((whoami) -like "nt authority\system") -and (([Environment]::UserInteractive) -eq $false) )  {
      if (!(Get-Module -ListAvailable -Name "RunAsUser")) {Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force > $null; Install-Module -Force -Name RunAsUser}
-     Write-Host "Detected non-interactive system user, launching '$exe' as current logged-in user instead. `n" 
      import-module RunAsUser
      if ($arguments) {$arguments = "?" + $arguments}
      $tempargument = "'" + "curl.exe $github/$exe" + $arguments + " | iex" + "'"
+     Write-Host "Detected non-interactive system user, launching '$tempargument' as current logged-in user instead. `n" 
      $scriptblock = [scriptblock]::Create("start-process powershell.exe -windowstyle hidden -argument $tempargument")
      invoke-ascurrentuser -nowait -scriptblock $scriptblock > $null
     } elseif ($_Admin -and $_Hidden) {
